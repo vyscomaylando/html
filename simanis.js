f3642f5e184c25c505f307e83f8f3ab109441638
2S Struggle Street <vyscomaylando@gmail.com>
2024-09-29 17:58:53 +0700

    Create simanis.js

---
+++ simanis.js
@@ -0,0 +1,347 @@
+
+
+<!DOCTYPE html>
+<html amp lang="id-ID" itemscope="itemscope" itemtype="https://schema.org/WebPage">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>LAPAK77 > Link Slot88 Deposit 5000 Gacor Peluang Gampang Menang Hari Ini</title>
+    <meta name="description" content="Lapak77 merupakan situs slot 88 deposit 5000 tanpa potongan dengan peluang gampang menang slot gacor hari ini yang telah menjadi salah satu game slot terpopuler digemari." />
+    <meta name="keywords" content="slot gacor, slot online gacor, slot gacor gampang menang, slot gacor resmi, slot gacor terpercaya." />
+    <link itemprop="mainEntityOfPage" rel="canonical" href="https://simanis.itn.ac.id/" />
+    <meta name="robots" content="index, follow" />
+    <meta name="page-locale" content="id,en">
+    <meta content="true" name="HandheldFriendly">
+    <meta content="width" name="MobileOptimized">
+    <meta content="indonesian" name="language">
+    <meta content='#007fa0' name='theme-color' />
+    <link rel="preload" as="image"href="https://iili.io/dZ6rt3X.png" />
+    <link rel="preload" as="script" href="https://cdn.ampproject.org/v0.js" />
+    <meta name="supported-amp-formats" content="websites,stories,ads,email">
+    <!-- Opengraph & Twitter -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="LAPAK77 > Link Slot88 Deposit 5000 Gacor Peluang Gampang Menang Hari Ini">
+    <meta name="twitter:description" content="Lapak77 merupakan situs slot 88 deposit 5000 tanpa potongan dengan peluang gampang menang slot gacor hari ini yang telah menjadi salah satu game slot terpopuler digemari.">
+    <meta name="twitter:image:src" content="https://iili.io/dZ6rt3X.png">
+    <meta name="twitter:player" content="https://youtu.be/">
+    <meta name="og:title" content="LAPAK77 > Link Slot88 Deposit 5000 Gacor Peluang Gampang Menang Hari Ini">
+    <meta name="og:description" content="Lapak77 merupakan situs slot 88 deposit 5000 tanpa potongan dengan peluang gampang menang slot gacor hari ini yang telah menjadi salah satu game slot terpopuler digemari.">
+    <meta name="og:image" content="https://iili.io/dZ6rt3X.png">
+    <meta property="og:image:width" content="600">
+    <meta property="og:image:height" content="466">
+    <meta name="og:url" content="https://simanis.itn.ac.id/">
+    <meta name="og:site_name" content="Slot Online Gacor">
+    <meta name="og:locale" content="ID_id">
+    <meta name="og:video" content="https://youtu.be/">
+    <meta name="og:type" content="website">
+    <meta property="og:type" content="video" />
+    <meta property="og:video:type" content="video/mp4">
+    <meta property="og:video:width" content="500">
+    <meta property="og:video:height" content="281">
+    <meta name="theme-color" content="#0a0a0a" />
+    <meta name="categories" content="SLOT GACOR" />
+    <meta name="language" content="ID">
+    <meta name="rating" content="general">
+    <meta name="copyright" content="Slot Online Gacor">
+    <meta name="author" content="Slot Online Gacor">
+    <meta name="distribution" content="global">
+    <meta name="publisher" content="Slot Online Gacor">
+    <meta name="geo.placename" content="DKI Jakarta">
+    <meta name="geo.country" content="ID">
+    <meta name="geo.region" content="ID" />
+    <meta name="tgn.nation" content="Indonesia">
+    <link rel="shortcut icon" type="image/x-icon" href="https://pub-22f5d56009c544d6a765cfa7997cd1a7.r2.dev/favicon2s.png" />
+    <link href='https://pub-22f5d56009c544d6a765cfa7997cd1a7.r2.dev/favicon2s.png' rel='icon' sizes='32x32' type='image/png' />
+    <script type="application/ld+json">
+        {
+          "@context": "https://schema.org",
+          "@type": "SoftwareApplication",
+          "name": "Slot Online Gacor",
+          "operatingSystem": "ANDROID",
+          "applicationCategory": "GameApplication",
+          "aggregateRating": {
+            "@type": "AggregateRating",
+            "ratingValue": "5",
+            "bestRating": "5",
+            "ratingCount": "1409980"
+          },
+          "review": {
+            "@type": "Review",
+            "reviewRating": {
+              "@type": "Rating",
+              "ratingValue": "5",
+              "bestRating": 5,
+              "worstRating": "1"
+            },
+            "author": {
+              "@type": "Person",
+              "name": "LAPAK77 > Link Slot88 Deposit 5000 Gacor Peluang Gampang Menang Hari Ini",
+              "reviewBody": "Lapak77 merupakan situs slot 88 deposit 5000 tanpa potongan dengan peluang gampang menang slot gacor hari ini yang telah menjadi salah satu game slot terpopuler digemari."
+            }
+          },
+          "offers": {
+            "@type": "Offer",
+            "price": " 10000.00",
+            "priceCurrency": "IDR"
+          }
+        }
+    </script>
+    <!-- <script async nomodule src="https://cdn.ampproject.org/v0.js" crossorigin="anonymous"></script> -->
+    <!-- <script async src="https://cdn.ampproject.org/v0/amp-carousel-0.1.mjs" custom-element="amp-carousel" type="module" crossorigin="anonymous"></script>
+    <script async nomodule src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js" crossorigin="anonymous" custom-element="amp-carousel"></script>
+    <script async src="https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.mjs" custom-element="amp-install-serviceworker" type="module" crossorigin="anonymous"></script>
+    <script async nomodule src="https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js" crossorigin="anonymous" custom-element="amp-install-serviceworker"></script>
+    <script async src="https://cdn.ampproject.org/v0/amp-youtube-0.1.mjs" custom-element="amp-youtube" type="module" crossorigin="anonymous"></script>
+    <script async nomodule src="https://cdn.ampproject.org/v0/amp-youtube-0.1.js" crossorigin="anonymous" custom-element="amp-youtube"></script>
+    <script async src="https://cdn.ampproject.org/v0/amp-accordion-0.1.mjs" custom-element="amp-accordion" type="module" crossorigin="anonymous"></script>
+    <script async nomodule src="https://cdn.ampproject.org/v0/amp-accordion-0.1.js" crossorigin="anonymous" custom-element="amp-accordion"></script> 
+         -->
+    <style id="NewLandingPage" amp-custom>
+        body {
+            background:#000000;
+            font-family: 'Open Sans', sans-serif;
+            color: #fff
+        }
+        .container {
+            width: 960px;
+            margin: 50px auto;
+        }
+        .logo {
+            text-align: left;
+        }
+        .logo img {
+            width: 100%;
+        }
+        .header {
+            display: grid;
+            grid-template-columns: 2fr 4fr;
+        }
+        .menu {
+            display: flex;
+            align-items: flex-end;
+            justify-content: flex-end;
+        }
+        .menu ul {
+            display: flex;
+            margin: 0px;
+            padding: 0px;
+            gap: 20px;
+        }
+        .menu ul li {
+            display: flex;
+            margin: 0px;
+            padding: 0px;
+        }
+        .menu ul li a {
+            color: #FFDA78;
+        }
+        a {
+            text-decoration: none;
+        }
+        a:hover {
+            text-decoration: none;
+        }
+        .main-content {
+            margin-top: 30px;
+            border-radius: 10px;
+            background: #111111;
+            padding: 15px;
+            display: block;
+        }
+        .banner {
+            text-align: center;
+            background: #1E1E1E;
+            border-radius: 10px;
+            margin-bottom: 30px;
+        }
+        .button-area {
+            text-align: center;
+            background: #1E1E1E;
+            border-radius: 10px;
+            margin-bottom: 30px;
+            display: grid;
+            grid-template-columns: 2fr 2fr;
+            gap: 15px;
+            padding:15px;
+        }
+        .btn-pluto {
+            display: flex;
+            padding: 20px;
+            background: #FFDA78;
+            color: #111111;
+            font-size: 16px;
+            text-transform: uppercase;
+            align-items: center;
+            justify-content: center;
+            font-weight: bold;
+            gap: 20px;
+        }
+        .icon-btn {
+            display: flex;
+            background: url(https://pub-22f5d56009c544d6a765cfa7997cd1a7.r2.dev/favicon2s.png);
+            background-repeat: no-repeat;
+            background-size: cover;
+            width: 25px;
+            height: 25px;
+            position: relative;
+            animation: blinker 1s linear infinite;
+        }
+
+        .copy {
+            margin-top: 30px;
+            border-radius: 10px;
+            background: #111111;
+            padding: 15px;
+            display: block;
+            color: #fff;
+            font-size: 14px;
+            text-align: center;
+        }
+
+*, *::before, *::after {
+	box-sizing: border-box;
+}
+
+@keyframes rotate {
+	100% {
+		transform: rotate(1turn);
+	}
+}
+
+.rainbow {
+	position: relative;
+	z-index: 0;
+	border-radius: 10px;
+	overflow: hidden;
+	padding: 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-family: sans-serif;
+	font-weight: bold;
+}
+.rainbow::before {
+    content: '';
+    position: absolute;
+    z-index: -2;
+    left: -50%;
+    top: -50%;
+    width: 200%;
+    height: 200%;
+    background-color: #399953;
+    background-repeat: no-repeat;
+    background-size: 50% 50%, 50% 50%;
+    background-position: 0 0, 100% 0, 100% 100%, 0 100%;
+    background-image: linear-gradient(#399953, #399953), linear-gradient(#fbb300, #fbb300), linear-gradient(#d53e33, #d53e33), linear-gradient(#377af5, #377af5);
+    animation: rotate 4s linear infinite;
+}
+.rainbow::after {
+    content: '';
+    position: absolute;
+    z-index: -1;
+    left: 3px;
+    top: 3px;
+    width: calc(100% - 6px);
+    height: calc(100% - 6px);
+    background: linear-gradient(#d49700, #bcbdbe);
+    border-radius: 5px;
+}
+
+        @keyframes blinker {
+            50% {
+                opacity: 0;
+            }
+        }
+
+      
+
+        @keyframes blinkings {
+            0% {
+                border: 2px solid #fff;
+            }
+
+            25% {
+                border: 2px solid #FFDA78;
+            }
+
+            50% {
+                border: 2px solid #00ffdd;
+            }
+
+            100% {
+                border: 2px solid #F00;
+            }
+        }
+        @media screen and (max-width: 960px) {
+            .container {
+                width: 100%;
+                padding: 30px 15px;
+            }
+            .header {
+                display: flex;
+                flex-direction: column;
+            }
+            .logo {
+                display: flex;
+                margin-bottom: 25px;
+                align-items: center;
+                justify-content: center;
+           }
+           .menu {
+            justify-content: center;
+            align-items: center;
+           }
+           .banner {
+            display: block;
+           }
+           .banner img {
+            width: 100%;
+            height:auto;
+           }
+           .button-area {
+                display: flex;
+                flex-direction: column;
+                gap: 25px;
+                width:100%;
+           }
+           .main-content {
+               width:100%;
+           }
+        }
+
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo">
+                <img src="https://pub-22f5d56009c544d6a765cfa7997cd1a7.r2.dev/2s-logo.png" alt="logo Slot Online Gacor" title="Logo Slot Online Gacor" width="200px">
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="https://simanis.itn.ac.id/">SLOT GACOR</a></li>
+                    <li><a href="https://simanis.itn.ac.id/">SLOT RESMI</a></li>
+                    <li><a href="https://simanis.itn.ac.id/">SLOT ONLINE</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="main-content">
+            <div class="banner">
+                <img src="https://iili.io/dZ6rt3X.png" alt="LAPAK77 > Link Slot88 Deposit 5000 Gacor Peluang Gampang Menang Hari Ini" title="LAPAK77 > Link Slot88 Deposit 5000 Gacor Peluang Gampang Menang Hari Ini" width="660px" height="660px">
+
+            </div>
+          
+
+            <div class="button-area">
+                <a href="https://t.ly/mainsite77" class="btn btn-pluto rainbow"><span class="icon-btn"></span>LAPAK77 LOGIN SLOT</a>
+                <a href="https://t.ly/mainsite77" class="btn btn-pluto rainbow"><span class="icon-btn"></span>LAPAK77 DAFTAR SLOT</a>
+            </div>
+        </div>
+
+        <div class="copy">
+            Copyright 2S STRUGGLE 2024
+        </div>
+    </div>
+</body>
+</html>
