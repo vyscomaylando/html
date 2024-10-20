191d11201de8597980062fdadac66f239f420ee5
2S Struggle Street <vyscomaylando@gmail.com>
2024-10-20 13:42:25 +0700

    Create gsa.js

---
+++ gsa.js
@@ -0,0 +1,471 @@
+
+<!DOCTYPE html>
+<html ⚡ lang="id">
+  <head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charset="utf-8" />
+    <title>Gacor88 Slot : Jam Situs Slot Online Gacor Pragmatic 777 Terpercaya</title>
+    <link rel="icon" type="image/x-icon" href="https://pub-22f5d56009c544d6a765cfa7997cd1a7.r2.dev/favicon2s.png" />
+    <link rel="canonical" href="https://creamyprime.com/" />
+    <meta property="og:site_name" content="Gacor88 Slot" />
+    <meta property="og:title" content="Gacor88 Slot : Jam Situs Slot Online Gacor Pragmatic 777 Terpercaya" />
+    <meta property="og:url" content="https://creamyprime.com/" />
+    <meta property="og:type" content="product" />
+    <meta property="og:description" content="Gacor88 slot adalah koleksi game slot online mahal dan gacor yang bervariasi menghibur. Slot pragmatic 777 menawarkan grafis yang memukau versi mobile yang inovatif sehingga meningkatkan peluang untuk meraih keberhasilan jackpot tertinggi." />
+    <meta property="og:image" content="https://iili.io/2935GnV.png" />
+    <meta property="og:image:width" content="1024" />
+    <meta property="og:image:height" content="1024" />
+    <meta property="product:price:amount" content="10000.00" />
+    <meta property="product:price:currency" content="IDR" />
+    <meta property="product:availability" content="instock" />
+    <meta itemprop="name" content="Gacor88 Slot : Jam Situs Slot Online Gacor Pragmatic 777 Terpercaya" />
+    <meta itemprop="url" content="https://creamyprime.com/" />
+    <meta itemprop="description" content="Gacor88 slot adalah koleksi game slot online mahal dan gacor yang bervariasi menghibur. Slot pragmatic 777 menawarkan grafis yang memukau versi mobile yang inovatif sehingga meningkatkan peluang untuk meraih keberhasilan jackpot tertinggi." />
+    <meta itemprop="thumbnailUrl" content="https://iili.io/2935GnV.png" />
+    <link rel="image_src" href="https://iili.io/2935GnV.png" />
+    <meta itemprop="image" content="https://iili.io/2935GnV.png" />
+    <meta name="twitter:title" content="Gacor88 Slot : Jam Situs Slot Online Gacor Pragmatic 777 Terpercaya" />
+    <meta name="twitter:image" content="https://iili.io/2935GnV.png" />
+    <meta name="twitter:url" content="https://creamyprime.com/" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:description" content="Gacor88 slot adalah koleksi game slot online mahal dan gacor yang bervariasi menghibur. Slot pragmatic 777 menawarkan grafis yang memukau versi mobile yang inovatif sehingga meningkatkan peluang untuk meraih keberhasilan jackpot tertinggi." />
+    <meta name="description" content="Gacor88 slot adalah koleksi game slot online mahal dan gacor yang bervariasi menghibur. Slot pragmatic 777 menawarkan grafis yang memukau versi mobile yang inovatif sehingga meningkatkan peluang untuk meraih keberhasilan jackpot tertinggi." />
+    <link rel="preconnect" href="https://images.squarespace-cdn.com" />
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
+    <script async custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>
+    <script async custom-element="amp-accordion" src="https://cdn.ampproject.org/v0/amp-accordion-0.1.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" type="text/css" />
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;700&display=swap" rel="stylesheet" />
+    <style amp-boilerplate>
+      body {
+        -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+      }
+      @-webkit-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-moz-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-ms-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-o-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+    </style>
+    <noscript>
+      <style amp-boilerplate>
+        body {
+          -webkit-animation: none;
+          -moz-animation: none;
+          -ms-animation: none;
+          animation: none;
+        }
+      </style>
+    </noscript>
+    <style amp-custom>
+      * {
+        box-sizing: border-box;
+      }
+      body {
+        background-color: #000;
+        color: #fff;
+        font-family: rubik, sans-serif;
+      }
+      header {
+        background-color: #000;
+        box-shadow: 0 0 9px 2px hsl(0deg 0% 4% / 48%);
+      }
+      header .header-wrapper {
+        margin: 0 auto;
+        max-width: 500px;
+      }
+      header .header-wrapper .logo {
+        display: flex;
+        justify-content: center;
+        position: relative;
+        padding: 5px 0;
+        margin-top: 1rem;
+      }
+      h1,
+      h2,
+      h3 {
+        color: 6fbcff;
+      }
+      nav {
+        background-color: #181818;
+        padding-bottom: 2px;
+        border-bottom: 2px solid #af586a;
+      }
+      nav .menu {
+        position: relative;
+      }
+      nav .menu ul {
+        list-style-type: none;
+        position: relative;
+        margin: 0;
+        padding: 0;
+      }
+      nav .menu ul li {
+        color: #fff;
+        display: inline-block;
+      }
+      nav .menu ul li a {
+        color: #fff;
+        text-decoration: none;
+        padding: 10px 15px;
+        display: inline-block;
+      }
+      .active {
+        background: -webkit-linear-gradient(top, #08a31d 0%, #76db02 100%);
+      }
+      .container {
+        margin: 0 auto;
+        max-width: 500px;
+      }
+      .content {
+        margin: 5px 0;
+        padding: 5px;
+      }
+      .text-center {
+        text-align: center;
+      }
+      .title {
+        background-color: #181818;
+        padding: 15px;
+        font-size: 17px;
+      }
+      .thumbs {
+        position: relative;
+        padding-top: 5px;
+      }
+      .btn-body {
+        margin: 15px 0;
+        position: relative;
+        display: flex;
+      }
+      .btn-body .btn-items {
+        margin: 0 5px;
+        width: 100%;
+      }
+      .btn-body .btn-items a {
+        text-decoration: none;
+        width: 100%;
+        padding: 15px 5px;
+        color: #fff;
+        background: -webkit-linear-gradient(top, #af586a 0%, #000000 100%);
+        border-radius: 5px;
+        display: inline-block;
+        text-align: center;
+        overflow: hidden;
+        position: relative;
+      }
+      .btn-body .btn-items a::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #af586a;
+        opacity: 0;
+        -webkit-transition: 0.2s opacity ease-in-out;
+        transition: 0.2s opacity ease-in-out;
+      }
+      .sticky-bottom-menu .menu-items.menu-login .menu-icons {
+        position: absolute;
+        top: 0;
+        top: -62px;
+        right: 0;
+        left: 0;
+        width: 70px;
+        height: 70px;
+        border-radius: 50px;
+        background-color: #181818;
+        border: 3px solid #af586a;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: auto;
+        -webkit-animation: beat 0.5s infinite alternate;
+        animation: beat 0.5s infinite alternate;
+      }
+      .sticky-bottom-menu .menu-items.menu-login .menu-icons i {
+        font-size: 30px;
+        border-radius: 50px;
+        color: #fff;
+      }
+      .box-games-wrapper {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+      }
+      .box-games-wrapper .box-games {
+        width: 150px;
+        margin: 15px 5px;
+      }
+      .box-games-wrapper .box-games .games-img {
+        position: relative;
+        overflow: hidden;
+        padding: 7px;
+      }
+      .box-games-wrapper .box-games .games-name {
+        font-size: 12px;
+        background-color: #181818;
+        padding: 10px 5px;
+        text-align: center;
+        font-weight: 500;
+      }
+      .box-text {
+        padding: 5px;
+      }
+      .box-text a {
+        text-decoration: none;
+        color: hsl(0, 0%, 100%);
+      }
+      .content table {
+        width: 100%;
+        border-collapse: collapse;
+      }
+      .box-text table {
+        width: 100%;
+        text-align: center;
+        border-collapse: separate;
+        border: 1px solid #fff;
+      }
+      .content table th,
+      .content table td {
+        padding: 8px;
+        border: 1px solid #af586a;
+      }
+      .content table th {
+        background: #af586a -webkit-linear-gradient(bottom, #af586a 0%, #af586a 100%) repeat-x;
+        text-shadow: 0 1px 3px rgb(0 0 0 / 75%);
+      }
+      .content table {
+        width: 100%;
+        border-collapse: collapse;
+      }
+      .box-text table th,
+      td {
+        border: 1px solid #fff;
+        padding: 5px;
+      }
+      .mt-50 {
+        margin-top: 50px;
+      }
+      footer {
+        padding: 15px 5px;
+      }
+      footer .footer-text {
+        text-align: center;
+      }
+      .sticky-bottom-menu {
+        background: #181818;
+        color: #fff;
+        position: fixed;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        z-index: 997;
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        border-top: 2px solid #af586a;
+      }
+      .sticky-bottom-menu .menu-items {
+        text-align: center;
+        padding: 12px 5px 8px;
+        color: #fff;
+        position: relative;
+        text-decoration: none;
+      }
+      .sticky-bottom-menu .menu-items .menu-icons {
+        position: relative;
+      }
+      .sticky-bottom-menu .menu-items .menu-icons i {
+        font-size: 20px;
+      }
+      .sticky-bottom-menu .menu-items.menu-login {
+        position: relative;
+        z-index: 999;
+        align-self: flex-end;
+      }
+      h1,
+      p {
+        text-align: center;
+      }
+      h1 {
+        border-bottom: 2px solid #af586a;
+        padding-bottom: 8px;
+      }
+      @keyframes beat {
+        to {
+          transform: scale(1.2);
+        }
+      }
+      @keyframes animateTop {
+        0% {
+          -webkit-transform: translateX(100%);
+          transform: translateX(100%);
+        }
+        100% {
+          -webkit-transform: translateX(-100%);
+          transform: translateX(-100%);
+        }
+      }
+      @keyframes animateRight {
+        0% {
+          -webkit-transform: translateY(100%);
+          transform: translateY(100%);
+        }
+        100% {
+          -webkit-transform: translateY(-100%);
+          transform: translateY(-100%);
+        }
+      }
+      @keyframes animateLeft {
+        0% {
+          -webkit-transform: translateY(-100%);
+          transform: translateY(-100%);
+        }
+        100% {
+          -webkit-transform: translateY(100%);
+          transform: translateY(100%);
+        }
+      }
+      @keyframes animateBottom {
+        0% {
+          -webkit-transform: translateX(-100%);
+          transform: translateX(-100%);
+        }
+        100% {
+          -webkit-transform: translateX(100%);
+          transform: translateX(100%);
+        }
+      }
+      @media only screen and (max-width: 720px) {
+        .box-games-wrapper {
+          justify-content: center;
+        }
+        .box-games-wrapper .box-games {
+          width: 45%;
+        }
+        nav .menu ul li {
+          display: block;
+          text-align: center;
+          padding-bottom: 12px;
+        }
+        nav .menu ul li a {
+          width: 100%;
+        }
+        h1 {
+          font-size: 18px;
+        }
+        .article p {
+          font-size: 14px;
+        }
+      }
+      .article {
+        margin-bottom: 6rem;
+      }
+    </style>
+    <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
+  </head>
+  <body>
+    <header>
+      <div class="header-wrapper">
+        <div class="logo">
+          <a href="https://creamyprime.com/">
+            <amp-img src="https://pub-22f5d56009c544d6a765cfa7997cd1a7.r2.dev/logo2s-red.png" width="280" height="84" alt="Logo Gacor88 Slot" layout="fixed"></amp-img>
+          </a>
+        </div>
+      </div>
+    </header>
+    <section class="content">
+      <div class="container">
+        <div class="thumbs">
+          <amp-img src="https://iili.io/2935GnV.png" width="700" height="700" alt="Pasaran Togel Resmi" layout="responsive"></amp-img>
+        </div>
+        <div class="btn-body">
+          <div class="btn-items">
+            <a href="https://t.ly/googleadsense" rel="nofollow noreferrer" target="_blank" title="Daftar Gacor88 Slot">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <b>DAFTAR</b>
+            </a>
+          </div>
+          <div class="btn-items">
+            <a href="https://t.ly/googleadsense" rel="nofollow noreferrer" target="_blank" title="Gacor88 Slot Login">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <b>LOGIN</b>
+            </a>
+          </div>
+        </div>
+        <div class="article">
+          <h1>Gacor88 Slot : Jam Situs Slot Online Gacor Pragmatic 777 Terpercaya</h1>
+          <p>Gacor88 slot adalah koleksi game slot online mahal dan gacor yang bervariasi menghibur. Slot pragmatic 777 menawarkan grafis yang memukau versi mobile yang inovatif sehingga meningkatkan peluang untuk meraih keberhasilan jackpot tertinggi.</p>
+        </div>
+      </div>
+    </section>
+    <div class="sticky-bottom-menu">
+      <a class="menu-items" href="https://t.ly/googleadsense" target="_blank" rel="nofollow">
+        <div class="menu-icons"><i class="fas fa-home"></i></div>
+        <div class="menu-text">Home</div>
+      </a>
+      <a class="menu-items" href="https://t.ly/googleadsense" target="_blank" rel="nofollow">
+        <div class="menu-icons"><i class="fab fa-android"></i></div>
+        <div class="menu-text">Apps</div>
+      </a>
+      <a class="menu-items menu-login" href="https://t.ly/googleadsense" target="_blank" rel="nofollow">
+        <div class="menu-icons"><i class="fas fa-user-alt"></i></div>
+        <div class="menu-text">Daftar</div>
+      </a>
+      <a class="menu-items" href="https://t.ly/googleadsense" target="_blank" rel="nofollow">
+        <div class="menu-icons"><i class="fas fa-tags"></i></div>
+        <div class="menu-text">Bonus</div></a
+      >
+      <a class="menu-items" href="https://t.ly/googleadsense" target="_blank" rel="nofollow">
+        <div class="menu-icons"><i class="fas fa-comments"></i></div>
+        <div class="menu-text">Livechat</div>
+      </a>
+    </div>
+  </body>
+</html>
